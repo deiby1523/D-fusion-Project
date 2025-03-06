7ce91a14c244bb1d025ad64779bac92b3a04c256
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-03-06 14:31:59 -0500

    OnBoarding screen created

---
+++ app/src/main/java/com/uts/proyecto/MainActivity.java
@@ -0,0 +1,168 @@
+package com.uts.proyecto;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvNext;
+    private ViewPager viewPager;
+    private LinearLayout layoutDots;
+
+    private IntroPref introPref;
+    private int[] layouts;
+
+    private TextView[] dots;
+
+    private MyViewPagerAdapter viewPagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        introPref = new IntroPref(this);
+        introPref.setIsFirstTimeLaunch(false);
+        if (introPref.isFirstTimeLaunch()) {
+            launchLoginScreen();
+            finish();
+        }
+
+        tvNext = findViewById(R.id.tvNext);
+        viewPager = findViewById(R.id.viewPager);
+        layoutDots = findViewById(R.id.layoutDots);
+
+        layouts = new int[]{
+                R.layout.intro_one,
+                R.layout.intro_two,
+                R.layout.intro_three
+        };
+
+        tvNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    viewPager.setCurrentItem(current);
+                } else {
+                    launchLoginScreen();
+                }
+            }
+        });
+
+        viewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.addOnPageChangeListener(onPageChangeListener);
+
+        addBottomDots(0);
+    }
+
+    ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            if (position == layouts.length - 1) {
+                tvNext.setText("Empezar");
+            } else {
+                tvNext.setText("Siguiente");
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+        int[] activeColors = getResources().getIntArray(R.array.active);
+        int[] inActiveColors = getResources().getIntArray(R.array.inactive);
+        layoutDots.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226",0));
+            dots[i].setGravity(5);
+            dots[i].setTextSize(50);
+            dots[i].setTextColor(inActiveColors[currentPage]);
+            layoutDots.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[currentPage].setTextColor(activeColors[currentPage]);
+        }
+    }
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+        LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + 1;
+    }
+
+    private void launchLoginScreen() {
+        introPref.setIsFirstTimeLaunch(false);
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
+    }
+}
