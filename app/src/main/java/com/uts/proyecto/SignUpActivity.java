5f38aa74f58bb14cd4ac660bf159536eef31e869
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2025-04-09 12:57:45 -0500

    Firebase Authentication Working

--- app/src/main/java/com/uts/proyecto/SignUpActivity.java
+++ app/src/main/java/com/uts/proyecto/SignUpActivity.java
@@ -2,16 +2,44 @@ package com.uts.proyecto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText textName, textEmail, textPassword, textConfirmPassword;
+    Button signUpButton;
+    TextView loginButton;
+
+    FirebaseAuth auth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null)
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            // User already signed in
+            // redirecting to HomeActivity
+            Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +47,53 @@ public class SignUpActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
 
+        auth = FirebaseAuth.getInstance();
 
-        var btnLogin = findViewById(R.id.signUpText);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        textName = findViewById(R.id.textName);
+        textEmail = findViewById(R.id.textEmail);
+        textPassword = findViewById(R.id.textPassword);
+        textConfirmPassword = findViewById(R.id.textConfirmPassword);
+        signUpButton = findViewById(R.id.signInButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = String.valueOf(textEmail.getText());
+                password = String.valueOf(textPassword.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignUpActivity.this, "Ingresa un Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(SignUpActivity.this, "Ingresa una contraseña", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            // FirebaseUser user = auth.getCurrentUser();
+                            Toast.makeText(SignUpActivity.this, "Usuario registrado", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Error de Autenticación", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        // Button to Login Activity
+        loginButton = findViewById(R.id.signUpLink);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
@@ -29,7 +101,6 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
-
 
 
     }
